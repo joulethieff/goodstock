254ae5240c85f6343ec452c2061e0e6e244c7e19
joulethieff <80182560+joulethieff@users.noreply.github.com>
2023-01-07 14:02:46 +0900

    날짜입력테스트

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import streamlit as st
+import datetime
 
 st.title("""간단한 주식차트 보기(테슬라)""")
 
@@ -9,3 +10,8 @@ StockChart = StockData.history(period = '1d', start='2019-7-1',end='2021-12-25')
 
 st.line_chart(StockChart.Close)
 st.line_chart(StockChart.Volume)
+
+## Date Input
+
+today = st.date_input("날짜를 선택하세요.", datetime.datetime.now())
+the_time = st.time_input("시간을 입력하세요.", datetime.time())
