ff8ae24cddabf1175316828f293af1e6827013b3
joulethieff <80182560+joulethieff@users.noreply.github.com>
2023-01-08 23:08:04 +0900

    날짜 선택이 핸드폰에서 입력할때 핸드폰 키보드가 팝업이 되어서 불편함.

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ df = pd.DataFrame({"Time":[1, 2, 3, 4]})
 
 st.header('문수보리 택일')
 
-start_date = st.date_input('Enter start date', value=datetime.datetime(2019,7,6))
-
+#start_date = st.date_input('Enter start date', value=datetime.datetime(2019,7,6))
 #start_time = st.time_input('Enter start time', datetime.time(8, 45))
 
+daate = st.text_input('Movie title', '2023/01/01')
+tiime2 = st.text_input('Movie title', '2023/01/01')
+
+"""
 start = "00:00"
 end = "23:59"
 times = []
@@ -23,9 +26,9 @@ while now != end:
     now += datetime.timedelta(minutes=1)
 
 times.append(end.strftime("%H:%M"))
+"""
 
 
-st.multiselect('Departure hour:',times)
 
 
 # start_datetime = datetime.datetime.combine(start_date, start_time)
