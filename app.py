e4314d274d9e76afd58bbdf5dae0dc9e5afdcb91
joulethieff <80182560+joulethieff@users.noreply.github.com>
2023-01-08 23:11:16 +0900

    코드 일부 수정

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import datetime
 
-df = pd.DataFrame({"Time":[1, 2, 3, 4]})
+#df = pd.DataFrame({"Time":[1, 2, 3, 4]})
 
 
 st.header('문수보리 택일')
@@ -11,8 +11,8 @@ st.header('문수보리 택일')
 #start_date = st.date_input('Enter start date', value=datetime.datetime(2019,7,6))
 #start_time = st.time_input('Enter start time', datetime.time(8, 45))
 
-st.text_input('Movie title', '2023/01/01')
-st.text_input('Movie title', '2023/01/01')
+st.text_input('날짜를 입력하세요', '2023/01/01')
+st.text_input('시간을 입력하세요', '2023/01/01')
 
 """
 start = "00:00"
