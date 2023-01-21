a1a42c75f560bf25fc340d4e4c7f2d11616e715a
joulethieff <80182560+joulethieff@users.noreply.github.com>
2023-01-21 21:48:07 +0900

    테스트

--- app.py
+++ app.py
@@ -12,17 +12,15 @@ import pytz
 import datetime
 import streamlit as st                              # 스트림릿 임포트
 
-
 # initialize Nominatim API, 지명과 관련된 api
 geolocator = Nominatim(user_agent="geoapiExercises")
 obj_tzf = TimezoneFinder()
 
-### 위치에서 문자열 타임존 가져오기
-obj_tzf_result = obj_tzf.timezone_at(lng=location.longitude, lat=location.latitude) # 타임존 스트링으로 가져오기
-
 ### 스트림릿에서 날짜값들을 입력
 col1,col2 = st.columns([2,10])                                               # 년 월 일로 나눔
 
+
+##### 기본 정보 입력 ##
 name = st.text_input('이름', '홍길동')  # 이름을 입력
 byear = st.number_input('생년', min_value=0, max_value=3000, value=1980, step=1)  # 생년을 입력
 bmonth = st.number_input('생월', min_value=1, max_value=12, value=1, step=1)  # 생월을 입력
@@ -32,6 +30,9 @@ bmin = st.number_input('분', min_value=0, max_value=60, value=0, step=1)  # 분
 bsec = st.number_input('초', min_value=0, max_value=60, value=0, step=1)  # 초를 입력
 lad = st.text_input('태어난곳', '서울시')  # 위치명 입력
 location = geolocator.geocode(lad)                                      # 위치에서 위경도 값을 가져옴
+### 위치에서 문자열 타임존 가져오기
+obj_tzf_result = obj_tzf.timezone_at(lng=location.longitude, lat=location.latitude) # 타임존 스트링으로 가져오기
+
 
 ### 해당 날짜를 타임존에 맞게 변경함. 로컬라이즈
 seoul = pytz.timezone(obj_tzf_result)
